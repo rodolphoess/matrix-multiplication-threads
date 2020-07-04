735ed94abeb9f27a7196406e840cb0034d8dc3c8
Rodolpho Sousa <rodolpho.sousa@bancointer.com.br>
2020-07-04 18:06:55 -0300

    Multiplicação concorrente

--- src/br/matriz/concorrente/MultiplicacaoConcorrente.java
+++ src/br/matriz/concorrente/MultiplicacaoConcorrente.java
@@ -11,19 +11,15 @@ public class MultiplicacaoConcorrente {
         int[][] matrizC = new int[tamanhoMatrizInteiro][tamanhoMatrizInteiro];
 
         Long tempoInicial = System.currentTimeMillis();
-        new Thread() {
-
-            @Override
-            public void run() {
-                for (int i = 0; i < matrizA.length; i++) {
-                    for (int j = 0; j < matrizC[i].length; j++) {
-                        for (int k = 0; k < matrizA[i].length; k++) {
-                            matrizC[i][j] += matrizA[i][k] * matrizB[k][j];
-                        }
+        new Thread(() -> {
+            for (int i = 0; i < matrizA.length; i++) {
+                for (int j = 0; j < matrizC[i].length; j++) {
+                    for (int k = 0; k < matrizA[i].length; k++) {
+                        matrizC[i][j] += matrizA[i][k] * matrizB[k][j];
                     }
                 }
             }
-        }.start();
+        }).start();
         Long tempoFinal = System.currentTimeMillis();
 
         String path = "../matrix-multiplication-threads/src/br/matriz/resultados/C" + tamanhoMatrizInteiro + "x" + tamanhoMatrizInteiro + "C.txt";
